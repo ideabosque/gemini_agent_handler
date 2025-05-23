d870b40bd7dd46aab722a777dab350b235638716
Idea Bosque <ideabosque@gmail.com>
2025-05-22 20:47:19 -0700

    address how to deal with the xml.

--- gemini_agent_handler/gemini_agent_handler.py
+++ gemini_agent_handler/gemini_agent_handler.py
@@ -570,14 +570,40 @@ class GeminiEventHandler(AIAgentEventHandler):
                             and len(accumulated_partial_text) > 500
                         ):
                             output_format = "text"
-                        self.send_data_to_stream(
-                            index=index,
-                            data_format=output_format,
-                            chunk_delta=accumulated_partial_text,
+                        if output_format == "xml":
+                            # Extract XML tags and text content
+                            xml_parts = []
+                            current_text = accumulated_partial_text
+                            while "<" in current_text:
+                                start_idx = current_text.find("<")
+                                if start_idx > 0:
+                                    xml_parts.append(current_text[:start_idx])
+                                end_idx = current_text.find(">", start_idx)
+                                if end_idx == -1:
+                                    xml_parts.append(current_text[start_idx:])
+                                    break
+                                xml_parts.append(current_text[start_idx : end_idx + 1])
+                                current_text = current_text[end_idx + 1 :]
+                            if current_text:
+                                xml_parts.append(current_text)
+
+                            for part in xml_parts:
+                                self.send_data_to_stream(
+                                    index=index,
+                                    data_format=output_format,
+                                    chunk_delta=part,
+                                )
+                                index += 1
+                        else:
+                            self.send_data_to_stream(
+                                index=index,
+                                data_format=output_format,
+                                chunk_delta=accumulated_partial_text,
+                            )
+                            index += 1
+                        accumulated_partial_text = (
+                            ""  # For non-XML content, use buffer threshold
                         )
-                        accumulated_partial_text = ""
-                        index += 1
-                # For non-XML content, use buffer threshold
                 elif len(accumulated_partial_text) >= int(
                     self.setting.get("accumulated_partial_text_buffer", "10")
                 ):
